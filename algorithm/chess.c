6f9ef46f4af348af3105f158114045800ca17da1
VFIW <yzi456789@126.com>
2018-04-26 12:52:54 +0800

    Version:2018/4/26

--- algorithm/chess.c
+++ algorithm/chess.c
@@ -9,7 +9,7 @@ struct
 int main (void) {
 
     for (i.a = 1; i.a <= 9; i.a++) {
-        for (i.b = 1; i.b<= 9; i.b++) {
+        for (i.b = 1; i.b <= 9; i.b++) {
             if((i.b - i.a) % 3) {
                 printf("A = %d,B = %d\n", i.a, i.b);
             }
