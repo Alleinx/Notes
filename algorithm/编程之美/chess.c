7f1efd0e2339c40c3ea794cfb0615d9ee8ef8e44
DP <yzi456789@126.com>
2018-04-26 13:08:44 +0800

    1:07,pm

---
+++ algorithm/编程之美/chess.c
@@ -0,0 +1,24 @@
+/**
+ * P13 : 中国象棋将帅问题
+ */
+
+#include <stdio.h>
+
+struct 
+{
+    unsigned char a:4;
+    unsigned char b:4;
+}i;
+/* 位域 */
+
+int main (void) {
+
+    for (i.a = 1; i.a <= 9; i.a++) {
+        for (i.b = 1; i.b<= 9; i.b++) {
+            if((i.b - i.a) % 3) {
+                printf("A = %d,B = %d\n", i.a, i.b);
+            }
+        }
+    }
+    return 0;
+}
