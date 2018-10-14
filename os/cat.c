0b65dd35d498372934894c752d1ebe1c971deff2
DP <yzi456789@126.com>
2018-10-14 14:44:25 +0800

    18/10/14 cat shell cmd

---
+++ os/cat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFSIZE 2048
+
+void readFile(FILE *);
+
+int main(int argc, char const *argv[])
+{
+    FILE *fp;
+    /* code */
+
+    if (argc == 1) {
+        readFile(stdin);
+    } else {
+        while(--argc) {
+            fp = fopen(*(++argv), "r+"); /* r+ for detecting if open a file or a directory */
+            if (fp){
+                readFile(fp);
+                fclose(fp);
+            } else {
+                fprintf(stderr, "Open failed: ");
+                perror(*argv); 
+            }
+        
+        }
+    }
+
+    return 0;
+}
+
+void readFile(FILE *fp) {
+    char buf[BUFSIZE];
+    while (fgets(buf, BUFSIZ, fp)) {
+        fputs(buf, stdout);
+    }
+}
