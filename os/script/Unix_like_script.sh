1ea97e685ee82af60fdf3a661d5eec9b5486bd38
DP <yzi456789@126.com>
2018-12-30 16:34:48 +0800

    unix script

--- os/script/Unix_like_script.sh
+++ os/script/Unix_like_script.sh
@@ -3,8 +3,14 @@ if [ $# -ne 3 ];then
     exit 1
 fi
 
+echo 'Print parameters here:'
+for para in $1 $2 $3;do
+    echo $para
+done
+
+echo
 if [ $1 -gt $2 ];then
-    echo $1 'is greater than' $2.
+    echo  $1 'is greater than' $2.
 elif [ $1 -eq $2 ];then
     echo $1 'is equal to' $2.
 else
@@ -29,15 +35,21 @@ else
     echo $3 'is not a file.'
 fi
 
-echo "Is it morning? Please answer yes or no"
+echo "Is it morning? Please answer yes or no:"
 read timeOfDay
 
 if [ "$timeOfDay" = "yes" ];then
-    echo "Good morning."
+    echo "  Good morning."
 elif [ "$timeOfDay" = "no" ];then
-    echo "Good sleep."
+    echo "  Good sleep."
 else
-    echo "nice try."
+    echo "  nice try."
 fi
 
+echo 'print /:'
+for stuff in $(ls /); do
+    echo '  ' $stuff
+done
+
 exit 0
+
