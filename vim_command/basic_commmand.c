8dd2bcae9a9bcb2b537e95388e763f680fd13d6d
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-03-06 10:19:33 +0800

    play vim

---
+++ vim_command/basic_commmand.c
@@ -0,0 +1,31 @@
+/**
+1. File Operation
+
+    1.1 open/create file
+        # vim file
+        1.1.1 close file
+            # :q
+            # ZZ
+    1.2 open a file in vim
+        # :open file
+    1.3 split window
+        # :split
+    1.4 close split
+        # :close
+    1.5 open another file in the same window
+        # :split ./filename
+    1.6 switch to next file
+        # :bn
+    1.7 switch to previouse file
+        # :bp
+
+2. Parenthesis pairing
+    2.1 # %
+
+3. Copy & paste
+    3.1 copy 
+        # yy
+    3.2 paste
+        # p //paste in current cursor.
+        # P //paste in current line.
+*/
