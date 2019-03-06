ccbb0d2334c19c6cfe6b1de85966e13acd54b257
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-03-06 10:52:19 +0800

    Basic_command: add more contents

--- vim_command/basic_commmand.c
+++ vim_command/basic_commmand.c
@@ -28,4 +28,25 @@
     3.2 paste
         # p //paste in current cursor.
         # P //paste in current line.
+
+4. Insertion
+    4.1 # I //Insert from the begining of the line.
+    4.2 # A //Insert from the end of the line.
+    4.3 # i //Insert at the cursor
+    4.4 # a //Insert (1) character after the cursor
+    4.5 # o //Insert a line after current line.
+    4.6 # O //Insert a line before current line.
+
+5. Search
+    5.1 # /text
+        5.1.1 n -> find next
+        5.1.2 N -> find previous
+
+        5.1.3 config
+            5.1.3.1 :set ignorecase //ignore case.
+            5.1.3.2 :set noignorecase //doesn't ignore cse
+            5.1.3.3 :set incsearch //search when start input characters, rather than hitting <enter>
+
 */
+
+
