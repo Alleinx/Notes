7794da39b8b388c704f87ded13cd6f4c05d38caa
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-07-26 23:29:40 +0800

    doc_Reads: Annotation.py modified

--- Python/doc_Reads/Annotation.py
+++ Python/doc_Reads/Annotation.py
@@ -3,3 +3,17 @@ def f(ham: str, eggs: str = 'eggs') -> str:
     print("arguments:", ham, eggs)
     return ham + ' and' + eggs
 
+def test(x: int, y: int) -> int:
+    x.append(10)
+    y.append(10)
+
+    return 'test'
+
+if __name__ == "__main__":
+
+    t1 = []
+    t2 = []
+    y = test(t1, t2)
+    
+    print(t1, t2)
+    print(type(y))
