ae8e92d0677f043c6deea3e50871c05fee40d055
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-08-03 14:46:08 +0800

    unitTest.py modified

--- Python/Book_Learning_python/python_crash_course/unitTest.py
+++ Python/Book_Learning_python/python_crash_course/unitTest.py
@@ -1,7 +1,11 @@
 import unittest
 
-def get_formatted_name(first, last):
-    full_name = first + ' ' + last
+def get_formatted_name(first, last, middle =''):
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+    else:
+        full_name = first + ' ' + last
+        
     return full_name.title()
 
 # Test class should extend from "unittest.TestCase"
@@ -13,6 +17,10 @@ class NameTestCase(unittest.TestCase):
         formatted_name = get_formatted_name('janis', 'joplin')
         # use assert here
         self.assertEqual(formatted_name, 'Janis Joplin')
+
+    def test_first_last_middle_name(self):
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
 
 if __name__ == "__main__":
     unittest.main()
