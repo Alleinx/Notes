78a081c885551bb9ea5d6f7a6e3a9f51dbd6ce6c
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-08-03 18:45:18 +0800

    alien invasion game: Add Moving Restriction and move config into settings.py

--- Python/Book_Learning_python/python_crash_course/part2/Alien_game/settings.py
+++ Python/Book_Learning_python/python_crash_course/part2/Alien_game/settings.py
@@ -6,3 +6,6 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230) #Gray
+
+        # Each time, the ship will move by 2 pixels.
+        self.ship_speed_factor = 3.5
--- Python/Book_Learning_python/python_crash_course/part2/Alien_game/ship.py
+++ Python/Book_Learning_python/python_crash_course/part2/Alien_game/ship.py
@@ -2,10 +2,11 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         # Set the initial position of the ship on screen.
         
         self.screen = screen
+        self.ai_settings = ai_settings
         
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -14,8 +15,9 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        self.__horizontal_var_offset = 5
-        self.__vertial_var_offset = 5
+        self.center = float(self.rect.centerx)
+
+        # Moving Flags:
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
@@ -26,14 +28,16 @@ class Ship():
         self.screen.blit(self.image, self.rect)
 
     def update(self):
-        if self.moving_right:
-            self.rect.centerx += self.__horizontal_var_offset
+        if self.moving_right and self.center < self.ai_settings.screen_width:
+            self.center += self.ai_settings.ship_speed_factor
 
-        if self.moving_left:
-            self.rect.centerx -= self.__horizontal_var_offset
+        if self.moving_left and self.center > 0:
+            self.center -= self.ai_settings.ship_speed_factor
 
-        if self.moving_up:
-            self.rect.bottom -= self.__vertial_var_offset
+        if self.moving_up and self.rect.bottom > 0:
+            self.rect.bottom -= self.ai_settings.ship_speed_factor
 
-        if self.moving_down:
-            self.rect.bottom += self.__vertial_var_offset
+        if self.moving_down and self.rect.bottom < self.ai_settings.screen_height:
+            self.rect.bottom += self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
