0ba28967bf158c0a8e0ee41431075b3d47d336ec
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-08-05 14:20:30 +0800

    refactor check_evnets func in game_functions.py

--- Python/Book_Learning_python/python_crash_course/part2/Alien_game/game_functions.py
+++ Python/Book_Learning_python/python_crash_course/part2/Alien_game/game_functions.py
@@ -9,32 +9,39 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-            
-            elif event.key == pygame.K_UP:
-                ship.moving_up = True 
-            
-            elif event.key == pygame.K_DOWN:
-                ship.moving_down = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
+            check_keyup_events(event, ship)
 
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+def check_keydown_events(event, ship):
+    """React when user press down a key"""
+
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+    elif event.key == pygame.K_UP:
+        ship.moving_up = True     
+
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+
+def check_keyup_events(event, ship):
+    """React when user release a key"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
             
-            elif event.key == pygame.K_UP:
-                ship.moving_up = False 
+    elif event.key == pygame.K_UP:
+        ship.moving_up = False 
             
-            elif event.key == pygame.K_DOWN:
-                ship.moving_down = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
 
 def update_screen(ai_settings, screen, ship):
     # Change the color of the screen for every iteration.
