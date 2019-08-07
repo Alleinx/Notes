e9dd7c694c775e6bded3b5b6c32ad0af06b3da18
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-08-07 17:53:48 +0800

    priority of (non)data descriptor

--- Python/doc_Reads/descriptor.py
+++ Python/doc_Reads/descriptor.py
@@ -15,11 +15,20 @@ class Car():
 class MyClass():
     # This is a class attribute(shard accross all instance of the class).
     car_inventory = Car(100)
+    
+    def __init__(self):
+        self.car_inventory = 'abc'
 
     # Note: Different from
         # def __init__(self):
         #     self.car_inventory = Car(100)
 
+
+# The look-up priority of attribute changes for *data descriptor* and *non-data descriptor*
+
+# delete the __set__() method to see the difference.
+# for non-data D: self.attribute has higher priority.
+# for data D: D obj has higher priority.
 
 m1 = MyClass()
 print(m1.car_inventory)
