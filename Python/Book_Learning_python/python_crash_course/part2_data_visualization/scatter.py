b4e3f509a93d98de9a055b3e3020bf8eb1402268
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-08-10 17:38:05 +0800

    scatter.py modified

--- Python/Book_Learning_python/python_crash_course/part2_data_visualization/scatter.py
+++ Python/Book_Learning_python/python_crash_course/part2_data_visualization/scatter.py
@@ -7,15 +7,19 @@ plt.ylabel("y value", fontsize=14)
 x = [i for i in range(1, 1001)]
 y = [i**2 for i in x]
 
+# ----------------------------
+# Color & size
 # plt.scatter(x, y, s=1, c = [(0,0.7,0.7)],edgecolors=None)
 # s: size of a point.
 # c: RGB color, should use a 2-D array with a single row if you
 #       really want to specify the same RGB/RGBA value for all points.
 #       the value should land between 0 ~ 1.
 
-# Or we can use color mapping
+# ----------------------------
+# edge color & color mapping
 # c=y: change the color with the value of y.
 # cmap: tell python which color to use for color mapping.
+#   matplotlib.org -> examples -> color examples -> colormaps_reference for more color.
 # edgecolors = 'none': remove the edge color of a point.
 plt.scatter(x, y, c=y, cmap=plt.cm.Blues, edgecolors='none', s = 3)
 
@@ -24,3 +28,11 @@ plt.scatter(x, y, c=y, cmap=plt.cm.Blues, edgecolors='none', s = 3)
 plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
+# ----------------------------
+# Save Img:
+# need to replace plt.show() with plt.savefig(), otherwise will give empty img.
+
+# plt.savefig('squaares_plot.png', bbox_inches='tight')
+
+# bbox_inches: remove empty space on the img, if want to reserve the empty space,
+#   just ignore this parameter.
