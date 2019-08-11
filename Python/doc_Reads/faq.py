d59722453aae2f82630f2d3539bf31470b1093a9
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-08-11 21:24:05 +0800

    faq: updated

--- Python/doc_Reads/faq.py
+++ Python/doc_Reads/faq.py
@@ -4,10 +4,7 @@ def lazy_import(*some_arg):
     'of the lower level module. However, it\'s better to rearrange the module level.')
     print(sys.version)
 
-if __name__ == "__main__":
-    lazy_import()
-
-
+# func obj feature: memory/cache
 def expensive(arg1, arg2, *, _cache={}):
     """Callers can only provide two parameters and optionally pass _cache by keyword"""
     if (arg1, arg2) in _cache:
@@ -18,3 +15,20 @@ def expensive(arg1, arg2, *, _cache={}):
     # and store result in the cache
     #_cache[(arg1, arg2)] = result
     # return result
+
+# passing optional parameter and keyword parameter from 1 func to another.
+def f(*args, **kwargs):
+    # passing to another function
+    print(type(args))
+    g(*args, **kwargs)
+    
+def g(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+if __name__ == "__main__":
+    # lazy_import()
+    f((1,2,3,4),5,6,7, key='map')
+    
+    
