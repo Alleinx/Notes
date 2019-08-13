49ceba7f7dfd4074882ee4bb7402ed0b0ff098cb
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-08-13 14:37:51 +0800

    cols.py: add comments

--- Python/Book_Learning_python/fluent_python/cp1/cols.py
+++ Python/Book_Learning_python/fluent_python/cp1/cols.py
@@ -9,18 +9,28 @@ class FrenchDeck:
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
                                             for rank in self.ranks]
-                                            
+
+    # Invoked by interpreter when execute len(obj)
     def __len__(self):
         return len(self._cards)
 
+    # Invoked by interpreter when execute obj[position]
     def __getitem__(self, position):
         return self._cards[position]
 
+    # Invoked by interpreter when execute print(obj)
     def __repr__(self):
         return str(self._cards)
 
+    # Invoked by interpreter when execute iterator
+    def __iter__(self):
+        return iter(self._cards)
+        # Or self._cards.__iter__()
 
 if __name__ == "__main__":
-    beer_card = FrenchDeck()
-    print(beer_card)
-    print(len(beer_card))
+    deck = FrenchDeck()
+    print(deck)
+    print(len(deck))
+    
+    for card in deck:
+        print(card)
