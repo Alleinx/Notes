c110ea41394f2e4c8a9ae9238c38131c93b17955
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-09-03 22:12:34 +0800

    func.py modified

--- Python/Book_Learning_python/fluent_python/func.py
+++ Python/Book_Learning_python/fluent_python/func.py
@@ -52,3 +52,27 @@ bingo = BingoCage(range(3))
 print(bingo.pick())
 # Directly invoke the class instance, and python will call __call__() method of BingoCage
 print(bingo())
+
+# --------------------
+# cls represents class, becaluse class is a keyword of python
+def tag(name, *content, cls=None, **attrs):
+    if cls is not None:
+        attrs['class'] = cls
+    
+    if attrs:
+        attr_str = ''.join(' {0} = {1}'.format(attr, value)
+                for attr, value in 
+                sorted(attrs.items()))
+    else:
+        attr_str = ''
+    
+    if content:
+        return '\n'.join('<%s%s>%s</%s>' % (name, attr_str, c, name) for c in content)
+    else:
+        return '<%s%s />' % (name, attr_str)
+
+print(tag('br'))
+print(tag('p', 'hello'))
+print(tag('p', 'hello', 'world'))
+print(tag('p', 'hello', id=33))
+print(tag('p', 'hello', 'world', cls='sidebar'))
