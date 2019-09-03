b1d9b567b88ed72dac269383b89182e89f477949
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-09-03 21:35:06 +0800

    func.py modified

--- Python/Book_Learning_python/fluent_python/func.py
+++ Python/Book_Learning_python/fluent_python/func.py
@@ -18,3 +18,37 @@ print(fact(5))
 # demonstrate feature #3
 maped_list = list(map(fact, range(10)))
 print(maped_list)
+
+# higher order function: A fucntion that takes other functions as parameter or returns function as its result.
+fruits = [str(i) for i in range(3, 1000, 25)]
+result = sorted(fruits, key=len)
+# sorted() is a higher order function.
+print(result)
+
+# -------------------------
+# Callable Object
+# All object of python could be callable if it defines the __call__ method.
+import random
+
+class BingoCage:
+    """
+    This class demonstrate how to define a callable class instance
+    """
+    def __init__(self, items):
+        self._items = list(items)
+        random.shuffle(self._items)
+
+    def pick(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError('Pick from empty Bingo Cage')
+    
+    def __call__(self):
+        return self.pick()
+
+bingo = BingoCage(range(3))
+# Normal invokation of class method:
+print(bingo.pick())
+# Directly invoke the class instance, and python will call __call__() method of BingoCage
+print(bingo())
