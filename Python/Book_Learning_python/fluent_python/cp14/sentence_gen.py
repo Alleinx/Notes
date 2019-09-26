818b46739cfa24ec29039263fbd3638966925606
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-09-26 16:38:22 +0800

    sentence_gen.py: modified comments and use lazy implementation

--- Python/Book_Learning_python/fluent_python/cp14/sentence_gen.py
+++ Python/Book_Learning_python/fluent_python/cp14/sentence_gen.py
@@ -8,15 +8,19 @@ class Sentence:
 
     def __init__(self, text):
         self.text = text
-        self.words = RE_WORD.findall(text)
     
     def __repr__(self):
         return 'Sentence(%s)' % reprlib.repr(self.text)
     
     def __iter__(self):
-        for word in self.words:
-            yield word
+        for match in RE_WORD.finditer(self.text):
+            yield match.group()
+        # return (match.group() for match in RE_WORD.finditer(self.text))
+        
         # Generator registor(Factory method) won't raise StopIteration Exception.
+        # Using Lazy Implementation:
+        # Unlike re.findall() (will return a list), re.finditer() will return a generator
+        # which will save a lot of memory.
 
 if __name__ == "__main__":
     def gen_AB():
