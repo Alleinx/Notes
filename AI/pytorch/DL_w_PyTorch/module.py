4795fbec03406038f0e84e8b3317fa5129d6339c
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-12-30 21:53:41 +0800

    DL_w_PyTorch: add module.py

---
+++ AI/pytorch/DL_w_PyTorch/module.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+x = torch.rand(3,2, dtype=torch.float)
+linear_layer = nn.Linear(2,1, bias=True)
+print(x)
+
+# Calling an instance of nn.Module ends up calling the 'forward()' method.
+# The forward() method executes the forward computation;
+# __call__() does sth else before and after calling forward().
+# Thus, DO NOT DIRECTLY CALL forward() in user code;
+print(linear_layer(x))
+print(linear_layer.weight)
+print(linear_layer.bias)
+
+# Briefly demonstrate the implementation of Module.call:
+# Lots of hooks won't get called properly if directly use .forward() in user code.
+def __call__(self, *input, **kwargs):
+    for hook in self._forward_pre_hooks.values():
+        hook(self, input)
+    
+    result = self.forward(*input, **kwargs)
+
+    for hook in self._forward_hooks.values():
+        hook_result = hook(self, input, result)
+        # ...
+    
+    for hook in self._backward_hooks.values():
+        pass
+        # ...
+    #...
+    return result
