9f3a212bc286217664ff3c859617ecd764f26f70
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-12-30 16:09:47 +0800

    progress.py updated

--- AI/pytorch/DL_w_PyTorch/progress.py
+++ AI/pytorch/DL_w_PyTorch/progress.py
@@ -1,4 +1,5 @@
 import torch
+
 def main(n_epochs, learning_rate, params, x, y):
     for epoch in range(1, n_epochs + 1):
         if params.grad is not None:
@@ -9,6 +10,29 @@ def main(n_epochs, learning_rate, params, x, y):
         loss.backward()
 
         params = (params - learning_rate * params.grad).detach().requires_grad_()
+
+        if epoch % 1000 == 0:
+            print('Epoch', epoch, 'Loss', loss)
+    
+    return params
+
+
+def main_w_optim(n_epochs, optimizer, params, x, y):
+    '''
+    Training progress with optimizer
+    1. Get predicted value with model
+    2. Calculate loss
+    3. Update Gradient
+      3.1 zero_grad() first.
+      3.2 call loss.backward().
+    4. Update Parameters with optimizer.step().
+    '''
+    for epoch in range(1, n_epochs + 1):
+        predicted = model(x, params)
+        loss = loss_fn(predicted, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
         if epoch % 1000 == 0:
             print('Epoch', epoch, 'Loss', loss)
@@ -31,6 +55,7 @@ if __name__ == "__main__":
     predicted = model(x, *params)
 
     # remember to zero out all gradients:
+    # zero_grad() method should be invoked before the .backwrad() method.
     optimizer.zero_grad()
 
     # calculate loss 
