596fe48cdb73bd14532914da4508c051c9f89c8e
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2020-02-26 18:52:14 +0800

    add core_application w/ GUI/code/tkhello2.py

--- Python/Book_Learning_python/core_application/GUI/code/tkhello2.py
+++ Python/Book_Learning_python/core_application/GUI/code/tkhello2.py
@@ -1,8 +1,21 @@
-import tkinter as tk
+from tkinter import *
 
-top = tk.Tk()
-top.geometry('500x500')
-quit = tk.Button(top, text='hello world',
-                 command=top.quit)
-quit.pack()
-tk.mainloop()
+
+def colourUpdate():
+    colour.set('red' if colour.get() != 'red' else 'blue')
+    print(colour.get())
+    l.configure(fg=colour.get())
+
+
+root = Tk()
+
+colour = StringVar()
+colour.set('red')
+
+button = Button(root, text="Click Me", command=colourUpdate)
+l = Label(root, textvariable=colour, fg=colour.get())
+
+l.pack()
+button.pack()
+
+root.mainloop()
