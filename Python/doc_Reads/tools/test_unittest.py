d5aca8223ba9c867a0ad04ec9fa6e3416d5c7940
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2020-03-10 00:38:47 +0800

    add dir tools w/test_unittest.py

---
+++ Python/doc_Reads/tools/test_unittest.py
@@ -0,0 +1,48 @@
+import unittest
+
+class TestStringMethod(unittest.TestCase):
+    def setUp(self):
+        print('setUp() method will be executed before every test method.')
+
+    def tearDown(self):
+        print('tearDown() method will be executed after every test method.')
+    
+    # Test method should start with 'test' prefix
+    def test_upper(self):
+        print('test_upper()')
+        # check whether two things are equal
+        self.assertEqual('foo'.upper(), 'FOO')
+    
+    def test_isupper(self):
+        # check conditions
+        print('test_isupper()')
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        print('test_split()')
+
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split() fails when the seperator is not a string
+
+        # Use assertRaises() to check whether a specific type of exception is raised.
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == "__main__":
+    # start testing
+    # unittest.main()
+
+    # or you can customize your testing workflow
+    # 1. define a suite
+    suite = unittest.TestSuite()
+    # 2. Add test method, the order of adding method is the execution order.
+    suite.addTest(TestStringMethod('test_upper'))
+    suite.addTest(TestStringMethod('test_split'))
+    suite.addTest(TestStringMethod('test_isupper'))
+
+    # 3. get a runner
+    runner = unittest.TextTestRunner()
+    # 4. perform test
+    runner.run(suite)
