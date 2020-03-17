88fd40cbf1c6917d9120ad99d25363ec4ca4b728
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2020-03-18 02:16:26 +0800

    files updated

--- Python/django/intro/first_app/views.py
+++ Python/django/intro/first_app/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world!')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+            'latest_question_list': latest_question_list,
+    }
+
+    return render(request, 'first_app/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}".format(question_id))
+
+def results(request, question_id):
+    response = "You're looking at the result of question {}".format(question_id)
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse("You're Voting on question {}".format(question_id))
