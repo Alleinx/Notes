51601ae76998e4563d833cece7149ea7bd3242b5
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2020-03-28 22:03:36 +0800

    updated

--- Python/doc_Reads/library/d_type/heapq_demo.py
+++ Python/doc_Reads/library/d_type/heapq_demo.py
@@ -4,6 +4,7 @@ import heapq
 import random
 import collections.abc
 
+
 def heapsort(h: collections.abc.Iterable):
     # Version1:
     result_heap = []
@@ -15,17 +16,26 @@ def heapsort(h: collections.abc.Iterable):
 
     return [heapq.heappop(result_heap) for _ in range(len(result_heap))]
 
-if __name__ == '__main__':
-    times = 15
-    h = [random.randint(0, 100) for _ in range(times)]
-    print(h)
-    # Construct a heap:
-    heapq.heapify(h)
-    print(h)
 
-    print('Smallest Value:', h[0])
+if __name__ == '__main__':
+    times = 20
+
+    # Indicate the priority of task
+    priority = [random.randint(0, 2) for _ in range(times)]
+    # Indicate the enqueue order of the task.
+    enqueue_order = [i for i in range(times)]
+    # Indicate the task itself
+    task = ['task' for _ in range(times)]
+
+    task_list = zip(priority, enqueue_order, task)
+
+    print(task_list)
+    # Construct a heap:
+    # heapq.heapify(h)
+    # print(h)
+    # print('Smallest Value:', h[0])
 
     # Perform heapsort:
-    result = heapsort(h)
+    result = heapsort(task_list)
     print('Results After heapsort', result)
     # Get the largest value of the heap
